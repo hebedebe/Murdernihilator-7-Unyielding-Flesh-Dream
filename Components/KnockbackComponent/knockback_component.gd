bffeb92401550a23410d5199130ac7380c9efd3c
hebedebe <hebedebe1@gmail.com>
2026-03-16 21:03:27 +1100

    kick fixed?

--- Components/KnockbackComponent/knockback_component.gd
+++ Components/KnockbackComponent/knockback_component.gd
@@ -6,11 +6,22 @@ signal knocked_back_no_args
 @export_category("Links")
 @export var velocity_target: Node
 
+func apply_velocity(target: Node, vel: Vector3) -> void:
+	if "velocity" in target:
+		target.velocity += vel
+	elif "linear_velocity" in target:
+		target.linear_velocity += vel
+
+func get_velocity(target: Node) -> Vector3:
+	if "velocity" in target:
+		return target.velocity
+	elif "linear_velocity" in target:
+		return target.linear_velocity
+	return Vector3.ZERO
+
 func knockback(strength: float, direction: Basis) -> void:
 	var impulse = direction * Vector3(0,0,-strength)
-	if "velocity" in velocity_target:
-		velocity_target.velocity += impulse
-	elif "linear_velocity" in velocity_target:
-		velocity_target.linear_velocity += impulse
+	apply_velocity(velocity_target, impulse)
 	knocked_back.emit(strength, direction)
 	knocked_back_no_args.emit()
+	
